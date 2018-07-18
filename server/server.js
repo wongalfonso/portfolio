96143569b1f1cb92ddf8d18ce72a0577edb4247d
wongalofonso <alfonsowongsd@gmail.com>
2018-07-18 02:06:47 -0700

    compressed image sizes

--- server/server.js
+++ server/server.js
@@ -5,32 +5,24 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
 const MongoStore = require('connect-mongo')(session);
-// const webpackDevMiddleware = require('webpack-dev-middleware');
 const axios = require('axios');
+const app = express();
 
-// if (process.env.NODE_ENV !== 'production') {require('dotenv').config();}
-// console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
-  console.log('yes');
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv')
+  dotenv.config();
 }
-console.log("NODE", process.env.NODE_ENV)
+
 mongoose.connect('mongodb://localhost/codewars' || process.env.MONGO_URL);
 mongoose.Promise = Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind('connection error:'));
 
-// const webpack = require('webpack');
-const app = express();
-// const config = require('../webpack.config.js');
-// const compiler = webpack(config);
 app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.static('dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(webpackDevMiddleware(compiler, {
-//   publicPath: config.output.publicPath
-// }));
 // app.use(session({
 //   secret: 'word hard',
 //   resave: true,
@@ -51,11 +43,11 @@ app.get("/api/weather/:input", (req, res) => {
 app.use('/api/users', require('./routes/Users'));
 app.use('/api/kyus', require('./routes/Kyus'));
 
-// app.get('*.js', (req,res, next) => {
-//   req.url = req.url + '.gz';
-//   res.set('Content-Encoding', 'gzip');
-//   next();
-// });
+app.get('*.js', (req,res, next) => {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('public', 'index.html'));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,7 @@ module.exports = {
       {
         test: /\.(png|jpg)$/,
         use: [
+          { loader: 'url-loader' },
           { loader: 'file-loader' },
         ]
       },
