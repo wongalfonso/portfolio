61117e1faf7bca1a05ea0b9bf4d59a6737d30269
wongalofonso <alfonsowongsd@gmail.com>
2018-11-17 20:58:27 -0800

    created a float grid for large screens

--- src/js/components/HomePage/About/About.jsx
+++ src/js/components/HomePage/About/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactGA from 'react-ga';
 import profileImage from '../../../../../public/images/mainProfileImage.png';
 
-const About = ({title}) => {
+const About = ({ title }) => {
 
   const downloadResume = () => {
     console.log('yes');
@@ -13,39 +13,37 @@ const About = ({title}) => {
   }
   return (
     <div className='about-page' id='aboutPage'>
-        <div className='container about-container' id='aboutContainer'> 
-          <div className="row">
-            <div className="about-header">
-                {title}              
-            </div>
-          </div>   
-          <div className="row about-row">
-            <div className = 'about-text'>
-              <div className = 'image-container'>
-                <img src = {profileImage} className = 'profile-image'/>
-              </div>
-            My name is <b>Alfonso Wong</b>. I graduated from Cal State Dominguez Hills with a bachelors in Digital Media with an emphasis in Audio Recording. After working as an Audio Engineer in San Diego, I found that I could use my love for creating and developing media and applying it to Web Design. After receiving my certification from Origin Code Academy, I have focused my time and energy into building Web Applications with the React framework. 
-            <br/>  
-            <span className = 'lg-paragraph'>
-              This site is built with just that. I have added React and Redux to this site because it is much more then just a static web page. This site is also hosted on AWS, which gives me flexibility with my application. I have basic CRUD operations on the back end that allow me to communicate via Mongoose to MongoDB. These CRUD operations allow me to login, post, edit and delete a Blog that I have created for my solutions of Code Wars that I have completed.
-            </span>     
-            <br/>
-            I hope you enjoy my page.
-            <br/>
-            <div className = 'about-name'>- Alfonso </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="download-btn">
-              <button 
-                id = 'downloadBtn' 
-                onClick = {downloadResume}>
-                DOWNLOAD RESUME
-              </button>
-            </div>
-          </div>
+      <div className="row">
+        <div className="about-header">
+          {title}
         </div>
       </div>
+      <div className="row about-row">
+        <div className='about-text'>
+          <div className='image-container'>
+            <img src={profileImage} className='profile-image' />
+          </div>
+          My name is <b>Alfonso Wong</b>. I graduated from Cal State Dominguez Hills with a bachelors in Digital Media with an emphasis in Audio Recording. After working as an Audio Engineer in San Diego, I found that I could use my love for creating and developing media and applying it to Web Design. After receiving my certification from Origin Code Academy, I have focused my time and energy into building Web Applications with the React framework.
+            <br />
+          <span className='lg-paragraph'>
+            This site is built with just that. I have added React and Redux to this site because it is much more then just a static web page. This site is also hosted on AWS, which gives me flexibility with my application. I have basic CRUD operations on the back end that allow me to communicate via Mongoose to MongoDB. These CRUD operations allow me to login, post, edit and delete a Blog that I have created for my solutions of Code Wars that I have completed.
+            </span>
+          <br />
+          I hope you enjoy my page.
+            <br />
+          <div className='about-name'>- Alfonso </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="download-btn">
+          <button
+            id='downloadBtn'
+            onClick={downloadResume}>
+            DOWNLOAD RESUME
+              </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
