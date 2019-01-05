1bfeb2ddba54be3174afe25ac7472324ee8240c7
wongalofonso <alfonsowongsd@gmail.com>
2019-01-04 22:43:17 -0800

    created food json

---
+++ public/files/food.js
@@ -0,0 +1,132 @@
+const menu = [
+  {
+    "coffees": [
+      {
+        "americano" :{
+          price: {
+            tall: 2.65,
+            grande: 3.25,
+            venti: 4.00
+          },
+          type: "hot coffee"
+        },
+        "hot coffee" :{
+          price: {
+            tall: 2.45,
+            grande: 3.05,
+            venti: 3.65
+          }, 
+          type: "hot coffee"
+        },
+        "caffe misto" :{
+          price: {
+            tall: 3.45,
+            grande: 4.00,
+            venti: 4.65
+          }, 
+          type: "hot coffee"
+        },
+        "caramel macchiatto" :{
+          price: {
+            tall: 3.25,
+            grande: 4.25,
+            venti: 4.75
+          },
+          type: "latte"
+        },
+        "white mocha" :{
+          price: {
+            tall: 3.35,
+            grande: 4.35,
+            venti: 4.80
+          },
+          type: "latte"
+        },
+        "mocha" :{
+          price: {
+            tall: 3.25,
+            grande: 4.25,
+            venti: 4.75
+          },
+          type: "latte"
+        },
+        "cinnamon dolce latte" :{
+          price: {
+            tall: 3.25,
+            grande: 4.25,
+            venti: 4.75
+          },
+          type: "latte"
+        },
+        "vanilla latte" :{
+          price: {
+            tall: 3.25,
+            grande: 4.25,
+            venti: 4.75
+          },
+          type: "latte"
+        },
+        "black and white mocha" :{
+          price: {
+            tall: 3.25,
+            grande: 4.35,
+            venti: 4.85
+          },
+          type: "latte"
+        },
+
+      }
+    ],
+    "food": [
+      {
+        breakfast: [
+          {
+            "bacon gouda" : {
+              price : 4.25
+            },
+            "carved ham and swiss" : {
+              price : 4.75
+            },
+            "double smoked bacon" : {
+              price : 4.75
+            },
+            "chicken sausage & bacon" : {
+              price : 4.50
+            },
+            "reduced-fat turkey bacon" : {
+              price : 4.25
+            },
+            "sausage, cheddar & egg" : {
+              price : 4.75
+            },
+          }
+        ],
+        bakery: [
+          {
+            "butter croissant" : {
+              price: 2.50
+            },
+            "cheese danish" : {
+              price: 2.75
+            },
+            "chocolate croissant" : {
+              price: 2.50
+            },
+            "classic coffee cake" : {
+              price: 2.00
+            },
+            "lemon loaf" : {
+              price: 3.50
+            },
+            "banana loaf" : {
+              price: 3.25
+            },
+            "old-fashioned doughnut" : {
+              price: 2.00
+            },
+          }
+        ]
+      }
+    ]
+  }
+]
