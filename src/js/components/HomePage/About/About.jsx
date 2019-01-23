4756b461823e4d181b1facd0747fdb6ff4a4261c
wongalofonso <alfonsowongsd@gmail.com>
2019-01-22 16:29:05 -0800

    adjusted about page for xs

--- src/js/components/HomePage/About/About.jsx
+++ src/js/components/HomePage/About/About.jsx
@@ -22,7 +22,7 @@ export default class About extends Component {
               <div className='image-container'>
                 <img src={profileImage} className='profile-image' />
               </div>
-              My name is <b>Alfonso Wong</b>. I graduated from Cal State Dominguez Hills with a bachelors in Digital Media with an emphasis in Audio Recording. After working as an Audio Engineer in San Diego, I found that I could use my love for creating and developing media and applying it to Web Design. After receiving my certification from Origin Code Academy, I have focused my time and energy into building Web Applications with the React framework.
+              My name is <i>Alfonso Wong</i>. I graduated from Cal State Dominguez Hills with a bachelors in Digital Media with an emphasis in Audio Recording. After working as an Audio Engineer in San Diego, I found that I could use my love for creating and developing media and applying it to Web Design. After receiving my certification from Origin Code Academy, I have focused my time and energy into building Web Applications with the React framework.
               <br />
               <span className='lg-paragraph'>
                 This site is built with just that. I have added React and Redux to this site because it is much more then just a static web page. This site is also hosted on AWS, which gives me flexibility with my application. I have basic CRUD operations on the back end that allow me to communicate via Mongoose to MongoDB. These CRUD operations allow me to login, post, edit and delete a Blog that I have created for my solutions of Code Wars that I have completed.
